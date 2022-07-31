51ddcce892f7eb970ede122c51f64579d9e94c7b
wangweiyong <2415056788@qq.com>
2022-07-31 14:02:45 +0800

    first commit

---
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+@EnableFeignClients
+//@EnableSwagger2
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
